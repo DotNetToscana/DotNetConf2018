eff595620331f5090fc0b17fccb1390c5e63570c
Marco Minerva <marco.minerva@gmail.com>
2018-10-14 12:41:59 +0200

    EmotionML demo

---
+++ AI on the edge/Start/EmotionML/EmotionML/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace EmotionML
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await Camera.StartAsync();
+            Camera.CameraHelper.FrameArrived += CameraHelper_FrameArrived;
+
+            base.OnNavigatedTo(e);
+        }
+
+        private async void CameraHelper_FrameArrived(object sender, FrameEventArgs e)
+        {
+        }
+    }
+}
